2da5dc1b98f7916d1a23f1a6fae80b8e3a9e2058
Arnaud TANGUY <arn.tanguy@gmail.com>
2018-07-12 10:09:31 +0200

    Licence under LGPL

--- include/gram_savitzky_golay/spatial_filters.h
+++ include/gram_savitzky_golay/spatial_filters.h
@@ -110,6 +110,7 @@ class RotationFilter
   Eigen::Matrix3d filter() const;
   bool ready() const
   {
+    std::cout << "rotation filter buffer size: " << buffer.size() << " / capacity " << buffer.capacity() << std::endl;
     return buffer.size() == buffer.capacity();
   }
 };
@@ -138,6 +139,7 @@ class TransformFilter
   }
   bool ready() const
   {
+    std::cout << "transformfilter::ready" << std::endl;
     return trans_filter.ready() && rot_filter.ready();
   }
 };
