8a9fec7fb62d06b08e2c45f8a313011ab1a4be11
Arnaud TANGUY <arn.tanguy@gmail.com>
2020-07-03 22:23:44 +0900

    Re-licence under BSD2

--- include/gram_savitzky_golay/spatial_filters.h
+++ include/gram_savitzky_golay/spatial_filters.h
@@ -1,20 +1,6 @@
-//  Copyright 2017-2018 CNRS-UM LIRMM
-//  Copyright 2017-2018 Arnaud TANGUY <arnaud.tanguy@lirmm.fr>
-//
-//  This file is part of robcalib.
-//
-//  robcalib is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  robcalib is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public License
-//  along with robcalib.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * Copyright 2017-2020 CNRS-UM LIRMM, CNRS-AIST JRL
+ */
 
 #pragma once
 
